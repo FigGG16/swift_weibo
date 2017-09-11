2d3407a42b0c9db334deec7a482d1afaac328a8b
KingGG <kinggg@FigGGdeiMac.local>
2017-09-10 21:10:28 -0700

    部署项目

---
+++ FX_WB/FX_WB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FX_WB
+//
+//  Created by KingGG on 2017/9/10.
+//  Copyright © 2017年 KingGG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
